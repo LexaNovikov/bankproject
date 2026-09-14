e308ae6e36507829bddf84c2edb3997e8cc3647a
LexaNovikov <lexaproxa228@gmail.com>
2026-09-14 15:54:59 +0300

    Прогнал test через black

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 from src.widget import get_date, mask_account_card
+
 print("==========MASK_ACCOUNT_CARD==========")
-with open("./mask_account_card.txt", "r")as file:
+with open("./mask_account_card.txt", "r") as file:
     data = file.readlines()
     for i in data:
-        print(i,mask_account_card(i), sep = "", end="\n=====================================\n")
+        print(i, mask_account_card(i), sep="", end="\n=====================================\n")
 print("\n=============GET_DATE=============")
 with open("./get_date_tests.txt", "r") as file:
     data = file.readlines()
     for i in data:
-        print(i, get_date(i), sep = "", end="\n=====================================\n")
+        print(i, get_date(i), sep="", end="\n=====================================\n")
