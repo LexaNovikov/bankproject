c9d1dd56ffe333e9cad1dfec0eede6a83fce90e1
LexaNovikov <lexaproxa228@gmail.com>
2026-09-15 16:06:15 +0300

    Добавил докстринги и изменил переменные

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,18 @@
 def filter_by_state(dictionaries_list: list[dict], key: str = "EXECUTED") -> list[dict]:
+    '''Функция для фильтрации логов по state'''
     result = []
-    for i in dictionaries_list:
-        if i["state"] == key:
-            result.append(i)
+    for data_dict in dictionaries_list:
+        if data_dict["state"] == key:
+            result.append(data_dict)
     return result
 
 
 def sort_by_date(dictionaries_list: list[dict], key: bool = True) -> list[dict]:
+    '''Функция для сортировки логов по дате'''
     len_of_result = 0
     result = []
-    for i in dictionaries_list:
-        result.append(i)
+    for data_dict in dictionaries_list:
+        result.append(data_dict)
         len_of_result += 1
         idx = len_of_result - 1
         while idx > 0 and result[idx - 1]["date"] < result[idx]["date"]:
