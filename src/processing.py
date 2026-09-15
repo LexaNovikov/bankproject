5a903eda9c699a224387f49d267b5d755363f0f1
LexaNovikov <lexaproxa228@gmail.com>
2026-09-15 13:58:18 +0300

    небольшой фикс processing

--- src/processing.py
+++ src/processing.py
@@ -16,6 +16,6 @@ def sort_by_date(dictionaries_list: list[dict], key: bool = True) -> list[dict]:
         while idx > 0 and result[idx - 1]["date"] < result[idx]["date"]:
             result[idx - 1], result[idx] = result[idx], result[idx - 1]
             idx -= 1
-    if key == False:
+    if not (key):
         result.reverse()
     return result
