a761c12ef3c0d2387f1e96fc012f1d9d16e91272
LexaNovikov <lexaproxa228@gmail.com>
2026-09-15 10:43:52 +0300

    фикс masks.py с помощью black

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
+
 def get_mask_account(account: str) -> str:
     """Функция маскировки номера аккаунта"""
     return "**" + account[-4:]
