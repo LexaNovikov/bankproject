42b13f2229850d5926b1662376524696fff3aef5
LexaNovikov <lexaproxa228@gmail.com>
2026-09-15 11:54:38 +0300

    Добавил модуль processing

---
+++ src/processing.py
@@ -0,0 +1,23 @@
+def filter_by_state(dictionaries_list: list[dict], key: str = "EXECUTED") -> list[dict]:
+    result = []
+    for i in dictionaries_list:
+        if i["state"] == key:
+            result.append(i)
+    return result
+
+
+def sort_by_date(dictionaries_list: list[dict], key: bool = True):
+    len_of_result = 0
+    result = []
+    for i in dictionaries_list:
+        result.append(i)
+        len_of_result += 1
+        idx = len_of_result - 1
+        while idx > 0 and result[idx - 1]["date"] < result[idx]["date"]:
+            result[idx - 1], result[idx] = result[idx], result[idx - 1]
+            idx -= 1
+
+    if key:
+        return result
+    else:
+        return reversed(result)
